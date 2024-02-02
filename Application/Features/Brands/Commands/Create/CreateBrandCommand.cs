06ffde1de72ae07ca9b7cb3ae93ba4d4516e7f34
BahadirAk <ak_bahad@hotmail.com>
2024-02-03 00:31:01 +0300

    CQRS and MediatR are added to project.

---
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -0,0 +1,19 @@
+using MediatR;
+
+namespace Application.Features.Brands.Commands.Create;
+
+public class CreateBrandCommand : IRequest<CreatedBrandResponse>
+{
+    public string Name { get; set; }
+    
+    public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
+    {
+        public async Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
+        {
+            CreatedBrandResponse createdBrandResponse = new();
+            createdBrandResponse.Id = Guid.NewGuid();
+            createdBrandResponse.Name = request.Name;
+            return createdBrandResponse;
+        }
+    }
+}
