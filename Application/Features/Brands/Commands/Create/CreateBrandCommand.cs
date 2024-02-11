58dd9fcfc274a838bee56c2b7dd1028c50ff7aac
BahadirAk <ak_bahad@hotmail.com>
2024-02-11 16:20:26 +0300

    Brand queries and commands are completed.

--- Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
+++ Application/Features/Brands/Commands/Create/CreateBrandCommand.cs
@@ -1,3 +1,6 @@
+using Application.Services.Repositories;
+using AutoMapper;
+using Domain.Entities;
 using MediatR;
 
 namespace Application.Features.Brands.Commands.Create;
@@ -8,11 +11,23 @@ public class CreateBrandCommand : IRequest<CreatedBrandResponse>
     
     public class CreateBrandCommandHandler : IRequestHandler<CreateBrandCommand, CreatedBrandResponse>
     {
+        private readonly IBrandRepository _brandRepository;
+        private readonly IMapper _mapper;
+
+        public CreateBrandCommandHandler(IBrandRepository brandRepository, IMapper mapper)
+        {
+            _brandRepository = brandRepository;
+            _mapper = mapper;
+        }
+
         public async Task<CreatedBrandResponse> Handle(CreateBrandCommand request, CancellationToken cancellationToken)
         {
-            CreatedBrandResponse createdBrandResponse = new();
-            createdBrandResponse.Id = Guid.NewGuid();
-            createdBrandResponse.Name = request.Name;
+            Brand brand = _mapper.Map<Brand>(request);
+            brand.Id = Guid.NewGuid();
+
+            await _brandRepository.AddAsync(brand);
+
+            CreatedBrandResponse createdBrandResponse = _mapper.Map<CreatedBrandResponse>(brand);
             return createdBrandResponse;
         }
     }
